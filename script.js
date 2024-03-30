3fa134062b35ba277c3aa0e112b560d884d22867
Tori00103 <vragucka@gmail.com>
2024-03-30 20:37:53 +0200

    add action with player changing

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const buttons = document.querySelectorAll("#btn");
-
+let player = 1;
 buttons.forEach(function(btn) {
     btn.addEventListener("click", function() {
-        if (btn.textContent === "x") {
-            console.log("o");
-            btn.textContent = "o";
-        } else {
-            console.log("x");
+        if (player === 1 && btn.textContent===" ") {
             btn.textContent = "x";
+            player++;
+        } else if (player === 2 && btn.textContent===" "){
+            btn.textContent = "o";
+            player = 1;
         }
     });
 });
